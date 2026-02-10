53ffc4b29dbbfd96e58465b661baaf4e1b6624d6
Maha <mahafathyh@gmail.com>
2026-02-10 23:59:42 +0200

    create login component

--- src/Components/Profile/Profile.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from "react";
-
-interface ProfileProps {}
-
-const Profile: FC<ProfileProps> = () => <>Profile Component</>;
-
-export default Profile;
---
+++ src/Pages/Login/Login.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+interface LoginProps {}
+
+const Login: FC<LoginProps> = () => <>Login Component</>;
+
+export default Login;
