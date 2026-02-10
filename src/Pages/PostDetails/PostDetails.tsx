844fc59c211be93f7eb38034668a7f547175a1f9
Maha <mahafathyh@gmail.com>
2026-02-11 00:21:35 +0200

    create PostDetails component

---
+++ src/Pages/PostDetails/PostDetails.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react';
+
+
+interface PostDetailsProps {}
+
+const PostDetails: FC<PostDetailsProps> = () => (
+  <div>
+    PostDetails Component
+  </div>
+);
+
+export default PostDetails;
