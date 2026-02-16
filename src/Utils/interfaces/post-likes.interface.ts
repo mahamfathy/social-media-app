64ec392028d3211e95ec31ff5dc2f06feb2d8f49
Maha <mahafathyh@gmail.com>
2026-02-16 21:59:36 +0200

    create post like interface

---
+++ src/Utils/interfaces/post-likes.interface.ts
@@ -0,0 +1,13 @@
+import type { ApiResponse } from "./api.interface";
+
+export interface Like {
+  _id: string;
+  name: string;
+  username?: string;
+  photo: string;
+  followersCount: number;
+  followingCount: number;
+  bookmarksCount: number;
+  id: string;
+}
+export type IPostLikes = ApiResponse<Like[]>;
