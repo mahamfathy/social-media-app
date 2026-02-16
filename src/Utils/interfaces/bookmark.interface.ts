47b689b74b8bd2fa3dcca0087a9967493d9ad260
Maha <mahafathyh@gmail.com>
2026-02-16 21:59:50 +0200

    create bookmard posts

---
+++ src/Utils/interfaces/bookmark.interface.ts
@@ -0,0 +1,8 @@
+import type { ApiResponse } from "./api.interface";
+
+export interface BookmarkPayload {
+  bookmarked: boolean;
+  bookmarksCount: number;
+}
+
+export type IBookMarkPost = ApiResponse<BookmarkPayload>;
