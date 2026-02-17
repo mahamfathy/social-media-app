e6aca1037dcae9a86f6f7f44637cad6373657d94
Maha <mahafathyh@gmail.com>
2026-02-18 00:51:04 +0200

    finish schema

--- src/Pages/Auth/Auth.schema.ts
+++ src/Pages/Auth/Auth.schema.ts
@@ -7,26 +7,32 @@ export const authSchema = z
       .min(3, "Name must be at least 3 characters")
       .max(20)
       .trim()
-      .optional(),
+      .optional()
+      .or(z.literal("")),
     username: z
       .string()
       .min(3, "Username must be at least 3 characters")
       .max(20)
-      .optional(),
+      .optional()
+      .or(z.literal("")),
     email: z
       .string()
       .min(1, "Email is required")
       .email("Please enter a valid email address")
-      .trim(),
+      .trim()
+      .toLowerCase(),
     password: z
       .string()
       .min(1, "Password is required")
+      .min(8, "Password must be at least 8 Char")
       .regex(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Password must has at least 1 small letter, capital letter, a number and a symbol ",
       ),
-    rePassword: z.string().min(1, "rePassword is required").optional(),
+    rePassword: z.string().min(1, "rePassword is required"),
     dateOfBirth: z
       .string()
+      .or(z.literal(""))
       .refine(
         (value) => new Date(value) < new Date(),
         "Date must be in the Past",
@@ -38,3 +44,5 @@ export const authSchema = z
     message: "Passwords don't match",
     path: ["rePassword"],
   });
+
+export type AuthSchema = z.infer<typeof authSchema>;
