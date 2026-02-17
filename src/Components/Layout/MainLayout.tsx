501eaaa5e4ae9a6a0da02f7eed89d7e43c90c947
Maha <mahafathyh@gmail.com>
2026-02-17 20:56:59 +0200

    edits in structure

--- src/Components/Layout/MainLayout.tsx
+++ src/Components/Layout/MainLayout.tsx
@@ -1,13 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Footer } from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 
 export const MainLayout = () => {
+  const location = useLocation();
   return (
     <>
-      <Navbar />
-      <Outlet />
-      <Footer />
+      {location.pathname === "/sign-in" || location.pathname === "/sign-up" ? (
+        <Outlet />
+      ) : (
+        <>
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
