2298a9d6846cdd049c71b1f77ec1c7c74647c5e1
Maha <mahafathyh@gmail.com>
2026-02-18 03:11:34 +0200

    finish api config

--- src/api/api.config.ts
+++ src/api/api.config.ts
@@ -1,16 +1,38 @@
 import axios from "axios";
+import toast from "react-hot-toast";
+
 const axiosInstance = axios.create({
   baseURL: "https://route-posts.routemisr.com",
-  timeout: 1000,
+  timeout: 5000,
 });
+
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-    if (token) config.headers.token = `Bearer ${token}`;
+    if (token) {
+      config.headers.token = `Bearer ${token}`;
+    }
     return config;
   },
   (error) => {
     return Promise.reject(error);
   },
 );
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      toast.error(error.response?.data?.message);
+      localStorage.removeItem("token");
+    } else {
+      const message = error.response?.data?.message || "Something went wrong";
+      toast.error(message);
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default axiosInstance;
