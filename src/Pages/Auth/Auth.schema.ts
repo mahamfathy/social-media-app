d6475210fecb8f29cbb57fb6fd650af5b0f17163
Maha <mahafathyh@gmail.com>
2026-02-18 03:09:52 +0200

    final schema

--- src/Pages/Auth/Auth.schema.ts
+++ src/Pages/Auth/Auth.schema.ts
@@ -2,6 +2,21 @@ import * as z from "zod";
 
 export const authSchema = z
   .object({
+    email: z
+      .string()
+      .min(1, "Email is required")
+      .email("Please enter a valid email address")
+      .trim()
+      .toLowerCase(),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(3, "Password must be at least 8 Char")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Password must has at least 1 small letter, capital letter, a number and a symbol ",
+      ),
+    rePassword: z.string().optional().or(z.literal("")),
     name: z
       .string()
       .min(3, "Name must be at least 3 characters")
@@ -15,34 +30,27 @@ export const authSchema = z
       .max(20)
       .optional()
       .or(z.literal("")),
-    email: z
-      .string()
-      .min(1, "Email is required")
-      .email("Please enter a valid email address")
-      .trim()
-      .toLowerCase(),
-    password: z
-      .string()
-      .min(1, "Password is required")
-      .min(8, "Password must be at least 8 Char")
-      .regex(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-        "Password must has at least 1 small letter, capital letter, a number and a symbol ",
-      ),
-    rePassword: z.string().min(1, "rePassword is required"),
     dateOfBirth: z
       .string()
+      .optional()
       .or(z.literal(""))
       .refine(
-        (value) => new Date(value) < new Date(),
+        (value) => !value || new Date(value) < new Date(),
         "Date must be in the Past",
-      )
-      .optional(),
+      ),
     gender: z.enum(["male", "female"]).optional(),
   })
-  .refine((data) => data.password === data.rePassword, {
-    message: "Passwords don't match",
-    path: ["rePassword"],
-  });
+  .refine(
+    (data) => {
+      if (data.rePassword) {
+        return data.password === data.rePassword;
+      }
+      return true;
+    },
+    {
+      message: "Passwords don't match",
+      path: ["rePassword"],
+    },
+  );
 
 export type AuthSchema = z.infer<typeof authSchema>;
