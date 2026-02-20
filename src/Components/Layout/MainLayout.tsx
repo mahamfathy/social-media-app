321ad90fca9546294b704bd10c2ca7c83235cfa3
Maha <mahafathyh@gmail.com>
2026-02-20 22:02:57 +0200

    apply token in main layout

--- src/Components/Layout/MainLayout.tsx
+++ src/Components/Layout/MainLayout.tsx
@@ -1,9 +1,10 @@
+import { useAuth } from "@/Utils/custom-hooks/useAuthContext/useAuth";
 import { Outlet } from "react-router-dom";
 import { Footer } from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
-
 export const MainLayout = () => {
-  const token = localStorage.getItem("token");
+  const { token } = useAuth();
+
   return (
     <>
       {token && <Navbar />}
