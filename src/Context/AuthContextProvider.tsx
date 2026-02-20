0fc458d94ac3a8da403d5605512865bcda6c52ec
Maha <mahafathyh@gmail.com>
2026-02-21 00:01:58 +0200

    edits

--- src/Context/AuthContextProvider.tsx
+++ src/Context/AuthContextProvider.tsx
@@ -1,17 +1,9 @@
-import { useEffect, useState, type ReactNode } from "react";
+import { useState, type ReactNode } from "react";
 import { AuthContext } from "./AuthContext";
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   const [token, setToken] = useState<string | null>(
     localStorage.getItem("token"),
   );
-
-  useEffect(() => {
-    if (token) {
-      localStorage.setItem("token", token);
-    } else {
-      localStorage.removeItem("token");
-    }
-  }, [token]);
 
   return (
     <AuthContext.Provider value={{ token, setToken }}>
