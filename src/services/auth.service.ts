b3fd868f77013e96f247aa87b83dccefaf076e1f
Maha <mahafathyh@gmail.com>
2026-02-23 01:59:06 +0200

    add getuserdata function

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import axiosInstance from "@/api/api.config";
 import type { AuthSchema } from "@/Pages/Auth/Auth.schema";
 import type { IAuth } from "@/Utils/interfaces/auth/auth.interface";
-
+import type { IUserData } from "@/Utils/interfaces/user/user-data.interface";
 export const AuthService = {
   login: async (values: Pick<AuthSchema, "email" | "password">) => {
     const { data } = await axiosInstance.post<IAuth>("/users/signin", values);
@@ -9,6 +9,10 @@ export const AuthService = {
   },
   register: async (values: AuthSchema) => {
     const { data } = await axiosInstance.post<IAuth>("/users/signup", values);
+    return data;
+  },
+  getUserData: async () => {
+    const { data } = await axiosInstance.get<IUserData>("/users/profile-data");
     return data;
   },
 };
