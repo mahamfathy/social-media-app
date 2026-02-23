5d6b52e8b3464d03440fd7d0cba63b071ca81c66
Maha <mahafathyh@gmail.com>
2026-02-23 02:00:14 +0200

    changes in type

--- src/Context/AuthContext.ts
+++ src/Context/AuthContext.ts
@@ -1,8 +1,11 @@
+import type { IUserData } from "@/Utils/interfaces/user/user-data.interface";
 import { createContext } from "react";
 
 export interface AuthContextType {
   token: string | null;
   setToken: (token: string | null) => void;
+  userData: IUserData | undefined;
+  setUserData: (userData: IUserData | undefined) => void;
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(
