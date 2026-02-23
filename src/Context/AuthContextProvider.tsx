2a2bc885aa76d8357d333b982c2588d8b6d6e963
Maha <mahafathyh@gmail.com>
2026-02-23 02:00:27 +0200

    add userdata state

--- src/Context/AuthContextProvider.tsx
+++ src/Context/AuthContextProvider.tsx
@@ -1,12 +1,28 @@
-import { useState, type ReactNode } from "react";
+import { AuthService } from "@/services/auth.service";
+import type { IUserData } from "@/Utils/interfaces/user/user-data.interface";
+import { useEffect, useState, type ReactNode } from "react";
 import { AuthContext } from "./AuthContext";
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   const [token, setToken] = useState<string | null>(
     localStorage.getItem("token"),
   );
+  const [userData, setUserData] = useState<IUserData | undefined>();
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const data = await AuthService.getUserData();
+        setUserData(data);
+      } catch (error) {
+        console.error("Fetch User Error:", error);
+      }
+    };
 
+    if (token) {
+      fetchUser();
+    }
+  }, [token]);
   return (
-    <AuthContext.Provider value={{ token, setToken }}>
+    <AuthContext.Provider value={{ token, setToken, userData, setUserData }}>
       {children}
     </AuthContext.Provider>
   );
