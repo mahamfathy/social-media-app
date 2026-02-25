a3ab526e9aacaebaac6dbcc11b118011d20efce6
Maha <mahafathyh@gmail.com>
2026-02-25 23:02:13 +0200

    create psorile service

---
+++ src/services/profile.service.ts
@@ -0,0 +1,11 @@
+import axiosInstance from "@/api/api.config";
+import type { IUserData } from "@/Utils/interfaces/user/user-data.interface";
+
+export const ProfileService = {
+  getProfile: async (userId: string) => {
+    const { data } = await axiosInstance.get<IUserData>(
+      `/users/${userId}/profile`,
+    );
+    return data;
+  },
+};
