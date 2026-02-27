9373ea9585ae643957e4b96c6674a716e759fe8a
Maha <mahafathyh@gmail.com>
2026-02-27 21:11:44 +0200

    edits in ui

--- src/Components/Layout/MainLayout.tsx
+++ src/Components/Layout/MainLayout.tsx
@@ -1,20 +1,16 @@
 import { useAuth } from "@/Utils/custom-hooks/useAuth/useAuth";
 import { Outlet } from "react-router-dom";
-import { Footer } from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 export const MainLayout = () => {
   const { token } = useAuth();
 
   return (
-    <>
+    <div className="flex min-h-screen flex-col bg-[#f0f2f5]">
       {token && <Navbar />}
-      <div className="bg-[#f0f2f5]">
-        <main className="min-w-0">
-          <Outlet />
-        </main>
-      </div>
 
-      {token && <Footer />}
-    </>
+      <main className="flex-1 min-w-0">
+        <Outlet />
+      </main>
+    </div>
   );
 };
