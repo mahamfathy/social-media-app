448049bdfd76e165f9f6b295e5215b50735b19c1
Maha <mahafathyh@gmail.com>
2026-02-28 01:17:59 +0200

    changes in function

--- src/Utils/custom-hooks/usePost/usePost.ts
+++ src/Utils/custom-hooks/usePost/usePost.ts
@@ -1,28 +1,13 @@
-import { postsService } from "@/services/posts.service";
 import { useQuery } from "@tanstack/react-query";
-import { useAuth } from "../useAuth/useAuth";
 
-export const usePost = (activeTab: string, userId?: string) => {
-  const { userData } = useAuth();
-
-  const myId = userData?.data?.user._id;
+export const usePost = (
+  key: any[],
+  fetchFn: () => Promise<any>,
+  enabled: boolean = true,
+) => {
   return useQuery({
-    queryKey: ["posts", activeTab, userId || myId],
-    queryFn: () => {
-      if (userId && userId !== myId) {
-        return postsService.getUserPosts(userId);
-      }
-      switch (activeTab) {
-        case "community":
-          return postsService.getFeed("all");
-        case "my posts":
-          return postsService.getFeed("me");
-        case "saved":
-          return postsService.getSavedPosts();
-        default:
-          return postsService.getFeed("following");
-      }
-    },
-    enabled: Boolean(myId) || Boolean(userId),
+    queryKey: key,
+    queryFn: fetchFn,
+    enabled: enabled,
   });
 };
